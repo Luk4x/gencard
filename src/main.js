e9a4b20eed65a0993e2295c7ee01750cbe810ec3
Luk4x <luk4xm4ci3l@gmail.com>
2022-10-23 11:13:44 -0300

    aula 1

--- src/main.js
+++ src/main.js
@@ -1,1 +1,19 @@
-import "./css/index.css"
+import './css/index.css';
+
+const cardBgColor1 = document.querySelector('.cc-bg svg > g g:nth-child(1) path');
+const cardBgColor2 = document.querySelector('.cc-bg svg > g g:nth-child(2) path');
+const cardLogo = document.querySelector('.cc-logo span:nth-child(2) img');
+
+const setCardType = type => {
+    const colors = {
+        visa: ['#436d99', '#2d57f2'],
+        mastercard: ['#df6f29', '#c69347'],
+        default: ['black', 'gray']
+    };
+
+    cardBgColor1.setAttribute('fill', colors[type][0]);
+    cardBgColor2.setAttribute('fill', colors[type][1]);
+    cardLogo.setAttribute('src', `cc-${type}.svg`);
+};
+globalThis.setCardType = setCardType;
+
